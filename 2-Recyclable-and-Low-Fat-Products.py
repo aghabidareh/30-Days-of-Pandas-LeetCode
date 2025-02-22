b20bc3144a7dd3a29191957094261b94f82dc140
aghabidareh <aghabidareh@gmail.com>
2025-02-22 06:26:59 -0800

    Recyclable and Low Fat Products

--- 2-Recyclable-and-Low-Fat-Products.py
+++ 2-Recyclable-and-Low-Fat-Products.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame(products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]['product_id'])
+    result = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]['product_id']
+    return pd.DataFrame(result)
